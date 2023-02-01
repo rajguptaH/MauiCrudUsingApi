0a2faa654e88ade3896a2e3c3a378964cee7c145
rajguptaH <rajkumar00999.rk@gmail.com>
2023-02-01 09:13:48 +0530

    Db Project Included

---
+++ MauiCrudUsingDapper.DB/dbo/Tables/Student.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Student] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Name]      VARCHAR (50)  NOT NULL,
+    [Class]     VARCHAR (50)  NOT NULL,
+    [RollNo]    INT           NOT NULL,
+    [Address]   VARCHAR (100) NOT NULL,
+    [IsDeleted] BIT           CONSTRAINT [DF_Student_IsDeleted] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_Student] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
